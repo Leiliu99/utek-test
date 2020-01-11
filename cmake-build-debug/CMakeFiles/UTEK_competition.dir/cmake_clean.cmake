c51b4c4288ca80820c5316a99605a598812aa25f
Leiliu99 <liulei.liu@mail.utoronto.ca>
2020-01-11 11:12:14 -0500

    ellie created it for test

---
+++ cmake-build-debug/CMakeFiles/UTEK_competition.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UTEK_competition.dir/main.cpp.o"
+  "UTEK_competition.pdb"
+  "UTEK_competition"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UTEK_competition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
